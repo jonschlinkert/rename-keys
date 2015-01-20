b932e011e7448dc173505870d9fd3af050489a9e
jonschlinkert <jon.schlinkert@sellside.com>
2015-01-20 06:45:02 -0500

    clean up tests

--- test.js
+++ test.js
@@ -8,25 +8,18 @@ describe('rename keys', function () {
     assert.deepEqual(rename({name: 'rename-keys'}), {name: 'rename-keys'});
   });
 
-  it('should add dashes to the name key.', function () {
-    var actual = rename({name: 'rename-keys'}, function(str) {
-      return '--' + str;
-    });
-    assert.deepEqual(Object.keys(actual)[0], '--name');
+  it('should return original object because nothing is renamed', function () {
+    assert.deepEqual(rename({a: 1}, function() {}), {a: 1});
   });
 
-  it('should add dashes to each key of package.json.', function () {
+  it('should rename keys.', function () {
     var actual = rename({name: 'rename-keys', description: 'foo'}, function(str) {
       return '--' + str;
     });
     assert.deepEqual(Object.keys(actual)[1], '--description');
   });
 
-  it('should return original object because nothing is renamed', function () {
-    assert.deepEqual(rename({a: 1}, function() {}), {a: 1});
-  });
-
-  it('should only rename keys which have to be renamed', function () {
+  it('should only rename keys that are updated:', function () {
     assert.deepEqual(rename({a: 1, b: 1}, function(key) {
       if (key === 'b') return 'c'
     }), {a: 1, c: 1});
