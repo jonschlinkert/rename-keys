cbcc0370e3148e617aa20edf2797b44cbd425c88
jonschlinkert <jon.schlinkert@sellside.com>
2015-01-20 00:46:41 -0500

    clean up, add test for no callback

--- test.js
+++ test.js
@@ -4,17 +4,21 @@ var assert = require('assert');
 var rename = require('./');
 
 describe('rename keys', function () {
-  var addDashes = function(str) {
-    return '--' + str;
-  };
+  it('should return the original object if no function is passed.', function () {
+    assert.deepEqual(rename({name: 'rename-keys'}), {name: 'rename-keys'});
+  });
 
   it('should add dashes to the name key.', function () {
-    var actual = rename({"name": "rename-keys"}, addDashes);
+    var actual = rename({name: 'rename-keys'}, function(str) {
+      return '--' + str;
+    });
     assert.deepEqual(Object.keys(actual)[0], '--name');
   });
 
   it('should add dashes to each key of package.json.', function () {
-    var actual = rename({"name": "rename-keys", "description": "foo"}, addDashes);
+    var actual = rename({name: 'rename-keys', description: 'foo'}, function(str) {
+      return '--' + str;
+    });
     assert.deepEqual(Object.keys(actual)[1], '--description');
   });
 
@@ -23,15 +27,15 @@ describe('rename keys', function () {
   });
 
   it('should only rename keys which have to be renamed', function () {
-    assert.deepEqual(rename({a: 1, b: 1}, function(prop) {
-      if (prop == 'b') return 'c'
+    assert.deepEqual(rename({a: 1, b: 1}, function(key) {
+      if (key === 'b') return 'c'
     }), {a: 1, c: 1});
   });
 
   it('should rename keys without conflicts', function () {
-    assert.deepEqual(rename({a: 1, b: 2, c: 3, d: 4}, function(prop) {
+    assert.deepEqual(rename({a: 1, b: 2, c: 3, d: 4}, function(key) {
       var renameMap = {a : 'd', b: 'c', c: 'b', d: 'a'};
-      return renameMap[prop];
+      return renameMap[key];
     }), {a: 4, b: 3, c: 2, d: 1});
   });
 });
